0bd725a8e1e32ef541c27f3eae057b31607f6db8
IgorSilvares <igorsilvares@gmail.com>
2023-07-18 18:43:17 +0200

    Add functionalities such as pagination, ordering and searching in HTML form using DRF.

--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+#from django.shortcuts import render
+from .models import MenuItem,Category
+from .serializers import MenuItemSerializer, CategorySerializer
 from rest_framework import generics
 
+class CategoryView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    ordening_fields = ['price','inventory']
+    filter_fields = ['price','inventory']
+    search_fields = ['title']
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
